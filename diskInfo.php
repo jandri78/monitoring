dd46f2f69ebf1bca03386ade070b141d2bc7f54d
Andres Perez <jandri78@gmail.com>
2017-08-04 09:33:30 -0500

    Update diskInfo.php

--- diskInfo.php
+++ diskInfo.php
@@ -6,4 +6,6 @@ header('Content-Type: application/json; charset=utf-8');
 
 echo $a;
 
+//df -Ph | awk '/^\// {print $1"\t"$2"\t"$4}' | python -c 'import json, fileinput; print json.dumps({"diskarray":[dict(zip(("mount", "spacetotal", "spaceavail"), l.split())) for l in fileinput.input()]}, indent=2)'
+
 ?>
